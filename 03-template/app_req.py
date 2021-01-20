155bda3aafa9fa66472106664fe1a5b2d3110a7e
Slotty <thanathipch9@gmail.com>
2021-01-20 11:01:27 +0700

    Updated POST method in BMI

--- 03-template/app_req.py
+++ 03-template/app_req.py
@@ -15,16 +15,24 @@ def user():
 
 @app.route('/bmi', methods=['GET','POST'])
 def BMI():
-    BMI = ''
-    if request.method == 'GET':
-        weight = request.args.get('weight',default = 0, type = int)
-        height = request.args.get('height', default=0, type=int)
-    elif request.method == 'POST':
-        weight = float(request.form.get('weight'))
-        height = float(request.form.get('height'))
-    
+ 
+    weight = request.args.get('weight', type=int)
+    height = request.args.get('height', type=float)
     BMI = calc_bmi(weight, height)
+
     return render_template('bmi.html', data=BMI)
+
+@app.route('/bmi2', methods=['POST'])
+def BMI2():
+
+    data = request.json
+    weight = data['weight']
+    height = data['height']
+    BMI = calc_bmi(weight, height)
+    return_data = {"weight":weight, "height":height, "bmi":BMI}
+    
+    return jsonify(return_data)
+
 
 def calc_bmi(weight, height):
     return round((weight / ((height / 100) ** 2)), 2)
